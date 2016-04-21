7e45f40c424feb5ed0a08cbf87982e8bdb1bf360
liuyiyang <870134657@qq.com>
2016-04-21 20:21:01 +0800

    Initial Commit

---
+++ 斗鱼TV/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  斗鱼TV
+//
+//  Created by stellar on 16/4/21.
+//  Copyright © 2016年 stellar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
